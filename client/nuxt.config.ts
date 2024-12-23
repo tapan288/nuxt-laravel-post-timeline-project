b8c5e772cc8bebeeda3cbbbde47278a5919bcae4
Tapan Sharma <tapansharma69@gmail.com>
2024-12-23 18:35:57 +0545

    install nuxt-auth-sanctum package

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
+
   devServer: {
     host: "backend.test",
+  },
+
+  modules: ["nuxt-auth-sanctum"],
+  sanctum: {
+    baseUrl: "http://backend.test:80", // Laravel API
   },
 });
