490c1deefe86ce6c3aa91627fb57b7507f2cb9f1
Tapan Sharma <tapansharma69@gmail.com>
2024-12-23 21:35:29 +0545

    making the first request

---
+++ client/pages/auth/login.vue
@@ -0,0 +1,22 @@
+<script setup>
+const { login: loginAction } = useSanctumAuth();
+const form = reactive({
+  email: "admin@admin.com",
+  password: "password",
+});
+
+const login = async () => {
+  await loginAction(form);
+};
+</script>
+
+<template>
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <input v-model="form.email" type="email" placeholder="Email" />
+      <input v-model="form.password" type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
