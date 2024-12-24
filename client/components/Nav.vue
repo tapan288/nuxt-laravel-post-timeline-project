4af9ede8cd40e8470f03d9eadb1e3a94dfc40493
Tapan Sharma <tapansharma69@gmail.com>
2024-12-24 20:30:52 +0545

    login/navigation designs

--- client/components/Nav.vue
+++ client/components/Nav.vue
@@ -1,12 +1,116 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="p-6 flex items-center justify-between">
-    <div class="flex items-center space-x-6">
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/dashboard">Dashboard</NuxtLink>
+  <header class="bg-white">
+    <nav
+      class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+    >
+      <div class="flex items-center gap-x-12">
+        <a href="#" class="-m-1.5 p-1.5">
+          <span class="text-xl font-bold"> Product Name </span>
+        </a>
+        <div class="hidden lg:flex lg:gap-x-12">
+          <a href="#" class="text-sm font-semibold leading-6 text-gray-900">
+            Dashboard
+          </a>
+        </div>
+      </div>
+      <div class="flex lg:hidden">
+        <button
+          type="button"
+          class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+        >
+          <span class="sr-only">Open main menu</span>
+          <svg
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+      </div>
+      <div class="hidden lg:flex">
+        <div class="flex items-center space-x-6">
+          <div class="text-sm font-semibold leading-6 text-gray-900">
+            User name
+          </div>
+          <button class="text-sm font-semibold leading-6 text-gray-900">
+            Log out &rarr;
+          </button>
+        </div>
+        <div>
+          <NuxtLink
+            to="/auth/login"
+            class="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Log in &rarr;
+          </NuxtLink>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Mobile menu -->
+    <div class="lg:hidden">
+      <div
+        class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:ring-1 sm:ring-gray-900/10"
+      >
+        <div class="flex items-center justify-between">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span class="text-xl font-bold"> Product Name </span>
+          </a>
+          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+            <span class="sr-only">Close menu</span>
+            <svg
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <div class="mt-6 flow-root">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="space-y-2 py-6">
+              <a
+                href="#"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+              >
+                Dashboard
+              </a>
+            </div>
+            <div class="py-6">
+              <div>
+                <button
+                  class="w-full text-left -mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Log out
+                </button>
+              </div>
+              <div>
+                <a
+                  href="#"
+                  class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="flex items-center space-x-6">
-      <NuxtLink to="/">Account</NuxtLink>
-    </div>
-  </div>
+  </header>
 </template>
--- client/pages/auth/login.vue
+++ client/pages/auth/login.vue
@@ -11,12 +11,67 @@ const login = async () => {
 </script>
 
 <template>
-  <div>
-    <h1>Login</h1>
-    <form @submit.prevent="login">
-      <input v-model="form.email" type="email" placeholder="Email" />
-      <input v-model="form.password" type="password" placeholder="Password" />
-      <button type="submit">Login</button>
-    </form>
+  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <h2
+        class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+      >
+        Log in
+      </h2>
+    </div>
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" @submit.prevent="login">
+        <div>
+          <label
+            for="email"
+            class="block text-sm font-medium leading-6 text-gray-900"
+            >Email address</label
+          >
+          <div class="mt-2">
+            <input
+              v-model="form.email"
+              id="email"
+              name="email"
+              type="email"
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+          <p class="mt-2 text-sm text-red-600" id="email-error">Error</p>
+        </div>
+        <div>
+          <label
+            for="password"
+            class="block text-sm font-medium leading-6 text-gray-900"
+            >Password</label
+          >
+          <div class="mt-2">
+            <input
+              v-model="form.password"
+              id="password"
+              name="password"
+              type="password"
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+          <p class="mt-2 text-sm text-red-600" id="email-error">Error</p>
+        </div>
+        <div>
+          <button
+            type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+      <p class="mt-10 text-center text-sm text-gray-500">
+        Not signed up?
+        <a
+          href="#"
+          class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >Register</a
+        >
+      </p>
+    </div>
   </div>
 </template>
