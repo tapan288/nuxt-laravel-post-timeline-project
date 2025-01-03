c9f77371c268cbbcc23b631f3909dd609e12e34e
Tapan Sharma <tapansharma69@gmail.com>
2025-01-03 21:10:13 +0545

    listening for events

--- client/pages/dashboard.vue
+++ client/pages/dashboard.vue
@@ -3,6 +3,12 @@ definePageMeta({
   middleware: ["sanctum:auth"],
   title: "Dashboard",
 });
+
+onMounted(() => {
+  Echo.channel("posts").listen("ExampleEvent", (e) => {
+    console.log(e);
+  });
+});
 </script>
 
 <template>
