d5ea060445516b1757fe47ccb341dbebd7dadf54
Tapan Sharma <tapansharma69@gmail.com>
2025-01-06 22:04:42 +0545

    work on the design

---
+++ client/components/CreatePost.vue
@@ -0,0 +1,24 @@
+<script setup>
+const body = ref("");
+</script>
+
+<template>
+  <form class="space-y-2">
+    <div>
+      <label for="body" class="sr-only">Post body</label>
+      <TextArea
+        class="w-full h-28 p-4 mt-1 block mb-2"
+        v-model="body"
+        placeholder="What do you want to say?"
+      />
+      <!-- <p
+            v-if="errors.body"
+            class="mt-2 text-sm text-red-600"
+            id="email-error"
+          >
+            {{ errors.body[0] }}
+          </p> -->
+    </div>
+    <PrimaryButton>Post</PrimaryButton>
+  </form>
+</template>
--- client/pages/dashboard.vue
+++ client/pages/dashboard.vue
@@ -16,7 +16,8 @@ onMounted(() => {
   <div class="py-12">
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 text-gray-900">
+        <div class="p-6 text-gray-900 space-y-8">
+          <CreatePost />
           <PostIndex />
         </div>
       </div>
